b3b66a27ad46fbfce36d4d25c2f715da916055ee
Sick9x <gekaav2001@gmail.com>
2023-05-11 04:14:14 +0600

    Added card_swiper package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:card_swiper/card_swiper.dart';
+import 'package:card_swiper/card_swiper.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Marvel Heroes'),
     );
   }
 }
