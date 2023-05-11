7914fb23bf0de28f411135a759af724471eab9f4
Sick9x <gekaav2001@gmail.com>
2023-05-11 06:34:47 +0600

    Lab 3: Added marvel API, images loading via network

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:card_swiper/card_swiper.dart';
+import 'package:dio/dio.dart';
+import 'dart:math' as math;
+
+const publicKey = "1eafe09deade90596441454ca854b684";
+const privateKey = "ca94959876c7a8cdcf240fc6b63202745043d1ab";
+const hash = "29fafe820f2207f78c76bb2f77addc57";
 
 void main() {
   runApp(const MyApp());
@@ -12,18 +18,63 @@ class MyApp extends StatefulWidget {
   State<StatefulWidget> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  List<String> nameList = ["Deadpool", "Iron man", "Spider man", "Thor"];
-  List<String> imageList = [
-    "assets/images/deadpool.jpg",
-    "assets/images/iron-man.jpg",
-    "assets/images/spider-man.jpg",
-    "assets/images/thor.jpg"
-  ];
-  List<Color> colorsList = [Colors.red, Colors.yellow,
-    Colors.cyan, Colors.white10
-  ];
+class Heroes {
+  String name;
+  String about;
+  String img;
 
+  Heroes({required this.name, required this.about, required this.img});
+
+  factory Heroes.fromJson(dynamic response) {
+    return Heroes(
+        name: response['name'] as String,
+        about: response['description'] as String,
+        img: response['thumbnail']['path'] +
+            '.' +
+            response['thumbnail']['extension'] as String);
+  }
+}
+
+class MarvelApi {
+  Future<List<int>> getIdHeroes(int count) async {
+    List<int> idHeroes = [];
+    String url = "https://gateway.marvel.com:443/v1/public/characters?";
+
+    try {
+      Response response = await Dio().get(url, queryParameters: {
+        "ts": 0,
+        "apikey": publicKey,
+        "hash": hash,
+        "limit": count
+      });
+
+      for (var dataHero in response.data["data"]["results"]) {
+        idHeroes.add(dataHero["id"]);
+      }
+
+      return idHeroes;
+    } catch (e) {
+      throw Exception("Exception: Could not get List of Heroes");
+    }
+  }
+
+  Future<Heroes> getInfoHeroes(int id) async {
+    String url =
+        "https://gateway.marvel.com:443/v1/public/characters/${id.toString()}?";
+
+    try {
+      Response response = await Dio().get(url,
+          queryParameters: {"ts": 0, "apikey": publicKey, "hash": hash});
+
+      var json = response.data["data"]["results"][0];
+      return Heroes.fromJson(json);
+    } catch (e) {
+      throw Exception("Exception: Could not get Hero");
+    }
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,7 +85,8 @@ class _MyAppState extends State<MyApp> {
             titleSpacing: 35,
             centerTitle: true,
             title:
-            Image.asset('assets/images/marvel.png', width: 400, height: 50),
+            Image.network('https://i.imgur.com/IANLc0p.png',
+                width: 400, height: 50),
             backgroundColor: Colors.transparent,
             elevation: 0),
         body: Stack(
@@ -50,42 +102,150 @@ class _MyAppState extends State<MyApp> {
                 ),
               ),
             ),
-            Swiper(
-              itemCount: imageList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                    padding: const EdgeInsets.symmetric(vertical: 45),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(20), // Image border
-                          child: SizedBox.fromSize(// Image radius
-                            child: Image.asset(imageList[index], fit: BoxFit.fill),
-                          ),
-                        ),
-                        Container(
-                          color: colorsList[index],
-                          alignment: Alignment.center,
-                          child: Text(
-                            nameList[index],
-                            style: const TextStyle(
-                                fontSize: 26,
-                                fontFamily: 'Marvel',
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    )
-                );
-              },
-              viewportFraction: 0.8,
-              scale: 0.9,
-            ),
+            FutureBuilder<List<int>>(
+                future: MarvelApi().getIdHeroes(10),
+                builder: ((context, list) {
+                  if (list.hasData) {
+                    return HeroSwiper(list.data!);
+                  }
+                  if (list.hasError) {
+                    return Text(
+                      list.error.toString(),
+                      style: const TextStyle(
+                        fontSize: 27,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    );
+                  }
+                  return const Center(child: CircularProgressIndicator());
+                })),
           ],
         ),
       ),
     );
   }
+}
+
+class HeroSwiper extends StatelessWidget {
+  List<int> heroList;
+  List<Color> colorsList = [Colors.red, Colors.yellow,
+    Colors.cyan, Colors.white10
+  ];
+
+  HeroSwiper(this.heroList, {super.key});
+
+  @override
+  Widget build(BuildContext context) => (Swiper(
+    itemCount: heroList.length,
+    itemBuilder: (BuildContext context, int index) {
+      return FutureBuilder<Heroes>(
+          future: MarvelApi().getInfoHeroes(heroList[index]),
+          builder: ((context, hero) {
+            if (hero.hasData) {
+              return GestureDetector(
+                onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => HeroCard(hero.data!),
+                )),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 45),
+                  child: Hero(
+                    tag: 'hero-${hero.data!.name}',
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20), // Image border
+                          child: SizedBox.fromSize(// Image radius
+                            child: Image.network(hero.data!.img, fit: BoxFit.fill, width: 1080, height: 500,),
+                          ),
+                        ),
+
+                        Container(
+                          color: Colors.cyan,
+                          alignment: Alignment.center,
+                          child: Text(
+                            hero.data!.name,
+                            style: const TextStyle(
+                                fontSize: 26,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.white70
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }
+            if (hero.hasError) {
+              return Center(child: Text(hero.error.toString()));
+            }
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.green,
+              ),
+            );
+          }));
+    },
+    viewportFraction: 0.8,
+    scale: 0.9,
+  ));
+}
+
+class HeroCard extends StatelessWidget {
+  final Heroes _heroes;
+  const HeroCard(this._heroes, {super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    extendBodyBehindAppBar: true,
+    appBar: AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+    ),
+    body: Hero(
+      tag: 'hero-${_heroes.name}',
+      child: Stack(
+        children: <Widget>[
+          Image.network(_heroes.img,
+              fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height),
+          Positioned(
+            bottom: 25,
+            left: 15,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5, left: 15),
+                    child: Text(
+                      _heroes.name,
+                      style: const TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 25, left: 15),
+                    child: Text(
+                      _heroes.about,
+                      style: const TextStyle(
+                        fontSize: 27,
+                        fontFamily: 'Marvel',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ]),
+          ),
+        ],
+      ),
+    ),
+  );
 }
